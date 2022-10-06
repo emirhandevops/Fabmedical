7dad310a134bde4b5d71fc5ada7963fcaaf46b1a
emirhandevops <eaydin@datamarket.com.tr>
2022-10-06 11:45:59 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("bf5ce22e-f760-4c78-bea4-cb3736dd4daf");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
